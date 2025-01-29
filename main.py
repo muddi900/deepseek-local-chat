f28e186a81b6c5d4ffc845c27f9bb271561f2964
Mudassir Chapra <muddi900@gmail.com>
2025-01-29 11:14:57 -0500

    The chat works, more or less.

--- main.py
+++ main.py
@@ -10,9 +10,23 @@ async def on_chat_start():
 @cl.on_message
 async def on_message(message: cl.Message):
     chat_history = cl.user_session.get("chat_history")
-    chat_history.append({
-        "role": "user",
-        "content": message.content
-    })
+    chat_history.append({"role": "user", "content": message.content})
 
-    
+    resp = cl.Message(content="")
+    chat_resp = ollama.chat(model="deepseek-r1:7b", messages=chat_history, stream=True)
+
+    final = ""
+    for cr in chat_resp:
+        t = cr["message"]["content"]
+        final += t
+        await resp.stream_token(t)
+
+    chat_history.append(
+        {
+            "role": "assistant",
+            "content": final,
+        }
+    )
+
+    cl.user_session.set("chat_history",chat_history)
+    await resp.send()
