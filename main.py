d1b0c9768e64dc5287f6a5824ed79b4557263891
Mudassir Chapra <muddi900@gmail.com>
2025-01-31 12:31:06 -0500

    Added dummy auth flow for data persistence[Issue #2]

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ import ollama
 from tqdm import tqdm
 
 import logging
+import asyncio
+
+import ollama
+import chainlit as cl
+
 
 if os.getenv("R1_VARIANT", "") not in ollama.ps().models:
     pull = ollama.pull(os.getenv("R1_VARIANT", "deepseek-r1:1.5b"), stream=True)
 
     for _ in tqdm(pull):
         pass
+
+
+@cl.password_auth_callback
+async def pass_auth(user: str, pwd: str):
+    return cl.User(
+        identifier="admin", metadata={"role": "admin", "provider": "credentrials"}
+    )
 
 
 @cl.on_chat_start
@@ -21,7 +33,6 @@ async def on_chat_start():
 async def on_message(message: cl.Message):
     chat_history = cl.user_session.get("chat_history")
     chat_history.append({"role": "user", "content": message.content})
-
     resp = cl.Message(content="")
 
     chat_resp = ollama.chat(
